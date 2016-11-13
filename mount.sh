5d987468bb32bda898a76f528018df9d1d4fa5a5
Harsh Lathwal <xeon.harsh@gmail.com>
2016-11-13 16:21:04 +0530

    Create mount.sh

---
+++ mount.sh
@@ -0,0 +1,18 @@
+#***************************************** 
+#* made By                               *
+#*    	 Xeon Zolt                       *
+#*	  	 aka Harsh Lathwal       *
+#*****************************************
+#its for guys who are on dual boot with winblows and forget to shut down winblows properly 
+#this script mount it in user home Dir in windows folder as readonly ( blame it on winblows )
+
+if [ ! -d "$windows" ]; then
+ cd $HOME 
+ mkdir windows
+fi
+
+# now selecting it the partition 
+# tested with windows 10 dual boot
+windows_part=$(sudo fdisk -l | grep "Microsoft basic data" | cut -d" " -f1)   
+# sudo mount -t ntfs-3g -o ro /dev/sdaX /windows  
+sudo mount -t ntfs-3g -o ro $windows_part /windows 
