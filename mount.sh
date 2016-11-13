15cfd57598ee2a03c0e076689bb08b1e22823682
Harsh Lathwal <xeon.harsh@gmail.com>
2016-11-13 16:28:50 +0530

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,11 +1,9 @@
 #its for guys who are on dual boot with winblows and forget to shut down winblows properly 
 #this script mount it in user home Dir in windows folder as readonly ( blame it on winblows )
-
-if [ ! -d "$windows" ]; then
+if [ ! -d "windows" ]; then
  cd $HOME 
  mkdir windows
 fi
-
 # now selecting it the partition 
 # tested with windows 10 dual boot
 windows_part=$(sudo fdisk -l | grep "Microsoft basic data" | cut -d" " -f1)   
